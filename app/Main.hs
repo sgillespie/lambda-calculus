4e462f285a63d8f56fd1f84c3a4539edb2703c7c
Sean Gillespie <sean@mistersg.net>
2016-09-23 08:24:04 -0400

    Update Main executable
     * Remove placeholder
     * Add a dummy repl

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,18 @@
 module Main where
 
-import Data.String.Strip
+import System.Console.Shell
+import System.Console.Shell.ShellMonad
+import System.Console.Shell.Backend.Readline (readlineBackend)
 
 main :: IO ()
-main = interact strip
+main = runShell shellDesc readlineBackend ()
+
+shellDesc :: ShellDescription ()
+shellDesc = mkShellDescription commands eval
+
+commands :: [ShellCommand s]
+commands = [exitCommand "q",
+            helpCommand "h"]
+
+eval :: String -> Sh st ()
+eval = shellPutStrLn
