e51f4b4c3aafd770dfaa1114000ed423300527b7
Sean Gillespie <sean@mistersg.net>
2016-09-24 13:56:28 -0400

    Update parser
     * Add Abstraction parser
     * Update whitespace handling

--- src/Language/Lambda/Parser.hs
+++ src/Language/Lambda/Parser.hs
@@ -1,31 +1,41 @@
 module Language.Lambda.Parser (parseExpr) where
 
+import Control.Monad
+import Prelude hiding (abs, curry, id)
+
 import Text.Parsec
 import Text.Parsec.String
 
 import Language.Lambda.Expression
 
 parseExpr :: String -> Either ParseError (LambdaExpr String)
-parseExpr = parse (expr <* eof) ""
+parseExpr = parse (whitespace *> expr <* eof) ""
 
 expr :: Parser (LambdaExpr String)
-expr = var <|> parens
+expr = abs <|> var <|> parens
   
 var :: Parser (LambdaExpr String)
 var = Var <$> identifier
 
+abs :: Parser (LambdaExpr String)
+abs = curry <$> idents <*> expr
+  where idents = (symbol '\\') *> many1 identifier <* (symbol '.')
+        curry = flip (foldr Abs)
+
 parens :: Parser (LambdaExpr String)
 parens = symbol '(' *> expr <* symbol ')'
-  
+
+lexeme :: Parser a -> Parser a
+lexeme p =  p <* whitespace
+
+whitespace :: Parser ()
+whitespace = void . many . oneOf $ " \t"
+
 identifier :: Parser String
-identifier = lexeme $ (:) <$> first <*> many rest
+identifier = lexeme ((:) <$> first <*> many rest)
   where first = letter <|> char '_'
         rest  = first <|> digit
 
-symbol :: Char -> Parser Char
-symbol = lexeme . char
+symbol :: Char -> Parser ()
+symbol = void . lexeme . char
 
-lexeme :: Parser a -> Parser a
-lexeme = (flip (<*)) . many $ whitespace
-  where whitespace :: Parser Char
-        whitespace = space <|> tab
--- test/Language/Lambda/ParserSpec.hs
+++ test/Language/Lambda/ParserSpec.hs
@@ -16,5 +16,25 @@ spec = do
     it "parses parenthesized variables" $ do
       parseExpr "(x)" `shouldBe` Right (Var "x")
 
+    it "parses parenthesized variables with whitespace" $ do
+      parseExpr "( x )" `shouldBe` Right (Var "x")
+
     it "does not parse trailing errors" $ do
       parseExpr "x +" `shouldSatisfy` isLeft
+
+    it "parses simple abstractions" $ do
+      parseExpr "\\x. x" `shouldBe` Right (Abs "x" (Var "x"))
+
+    it "parses nested abstractions" $ do
+      parseExpr "\\f a. a" `shouldBe` Right (Abs "f" (Abs "a" (Var "a")))
+  
+    it "ignores whitespace" $ do
+      let exprs = [
+            " x ",
+            " \\ x . x ",
+            " ( x ) "
+            ]
+      
+      mapM_ ((flip shouldSatisfy) isRight . parseExpr) exprs
+            
+
