c7304a22977ea16f8f1e3cb6b5171c31864513eb
Sean Gillespie <sean@mistersg.net>
2016-10-14 20:57:04 -0400

    Refactor evaluator
     * Add unique supply and freeVars
     * Add a freeVarsOf function
    
    This is all in preparation for wiring up the alpha converter

--- src/Language/Lambda/Eval.hs
+++ src/Language/Lambda/Eval.hs
@@ -6,15 +6,22 @@ import Data.Maybe
 import Language.Lambda.Expression
 
 evalExpr :: Eq n => LambdaExpr n -> LambdaExpr n
-evalExpr (App e1@(Var _) e2) = betaReduce e1 (evalExpr e2)
-evalExpr (App e1         e2) = betaReduce (evalExpr e1)  e2
-evalExpr (Abs name expr)     = Abs name (evalExpr expr)
-evalExpr e = e
+evalExpr expr = evalExpr' [] [] expr
 
-betaReduce :: Eq n => LambdaExpr n -> LambdaExpr n -> LambdaExpr n
-betaReduce (Abs n expr) = evalExpr . sub n expr
-betaReduce (App e1 e1') = App . betaReduce e1 $ e1'
-betaReduce expr@(Var _) = App expr
+evalExpr' :: Eq n => [n] -> [n] -> LambdaExpr n -> LambdaExpr n
+evalExpr' uniqs fvs (App e1@(Var _) e2)
+  = betaReduce uniqs fvs e1 (evalExpr' uniqs fvs e2)
+evalExpr' uniqs fvs (App e1         e2)
+  = betaReduce uniqs fvs (evalExpr' uniqs fvs e1)  e2
+evalExpr' uniqs fvs (Abs name expr)
+  = Abs name (evalExpr' uniqs fvs expr)
+evalExpr' _ _ e = e
+
+betaReduce :: Eq n => [n] -> [n]
+           -> LambdaExpr n -> LambdaExpr n -> LambdaExpr n
+betaReduce uniqs fvs (Abs n expr) = evalExpr' uniqs fvs . sub n expr
+betaReduce uniqs fvs (App e1 e1') = App . betaReduce uniqs fvs e1 $ e1'
+betaReduce _ _ expr@(Var _) = App expr
 
 alphaConvert :: Eq n => [n] -> [n] -> LambdaExpr n -> LambdaExpr n
 alphaConvert uniqs freeVars expr@(Abs name body)
@@ -27,8 +34,14 @@ sub :: Eq n => n -> LambdaExpr n -> LambdaExpr n -> LambdaExpr n
 sub name b@(Var name') expr | name == name' = expr
                             | otherwise = b
 
-sub name b@(Abs name' expr') expr | name == name' = b
-  | otherwise = Abs name' (sub name expr' expr)
+sub name b@(Abs name' expr') expr
+  | name == name' = b
+  | otherwise     = Abs name' (sub name expr' expr)
 
 sub name (App e1 e2) expr = App (sub name e1 expr)
                                 (sub name e2 expr)
+
+freeVarsOf :: Eq n => LambdaExpr n -> [n]
+freeVarsOf (Abs n expr) = filter (/=n) . freeVarsOf $ expr
+freeVarsOf (App e1 e2)  = freeVarsOf e1 ++ freeVarsOf e2
+freeVarsOf (Var n)      = [n]
--- test/Language/Lambda/EvalSpec.hs
+++ test/Language/Lambda/EvalSpec.hs
@@ -20,37 +20,45 @@ spec = do
       let expr = Abs "x" (App (Abs "y" (Var "y")) (Var "x"))
       evalExpr expr `shouldBe` Abs "x" (Var "x")
 
+    it "reduces with name captures" $ do
+      pending
+      
+      let expr = App (Abs "f" (Abs "x" (App (Var "f") (Var "x"))))
+                     (Abs "f" (Var "x"))
+      evalExpr expr `shouldBe` Abs "y" (Var "x")
+
   describe "betaReduce" $ do
+    let betaReduce' = betaReduce [] []
+    
     it "reduces simple applications" $ do
       let e1 = Abs "x" (Var "x")
           e2 = (Var "y")
-      betaReduce e1 e2 `shouldBe` Var "y"
+      betaReduce' e1 e2 `shouldBe` Var "y"
 
     it "reduces nested abstractions" $ do
       let e1 = Abs "x" (Abs "y" (Var "x"))
           e2 = Var "z"
-      betaReduce e1 e2 `shouldBe` Abs "y" (Var "z")
+      betaReduce' e1 e2 `shouldBe` Abs "y" (Var "z")
 
     it "reduces inner applications" $ do
       let e1 = Abs "f" (App (Var "f") (Var "x"))
           e2 = Var "g"
-      betaReduce e1 e2 `shouldBe` App (Var "g") (Var "x")
+      betaReduce' e1 e2 `shouldBe` App (Var "g") (Var "x")
 
     it "does not reduce unreducible expression" $ do
       let e1 = Var "x"
           e2 = Var "y"
-      betaReduce e1 e2 `shouldBe` App (Var "x") (Var "y")
+      betaReduce' e1 e2 `shouldBe` App (Var "x") (Var "y")
 
     it "does not reduce irreducible chained applications" $ do
       let e1 = App (Var "x") (Var "y")
           e2 = Var "z"
-      betaReduce e1 e2 `shouldBe` App (App (Var "x") (Var "y")) (Var "z")
+      betaReduce' e1 e2 `shouldBe` App (App (Var "x") (Var "y")) (Var "z")
 
     it "does not sub shadowed bindings" $ do
       let e1 = Abs "x" (Abs "x" (Var "x"))
           e2 = Var "z"
-      betaReduce e1 e2 `shouldBe` Abs "x" (Var "x")
-
+      betaReduce' e1 e2 `shouldBe` Abs "x" (Var "x")
 
   describe "alphaConvert" $ do
     it "alpha converts simple expressions" $ do
@@ -64,3 +72,16 @@ spec = do
           expr = Abs "x" (Var "x")
           uniques = ["x", "y"]
       alphaConvert uniques freeVars expr `shouldBe` Abs "y" (Var "y")
+
+  describe "freeVarsOf" $ do
+    it "Returns simple vars" $ do
+      freeVarsOf (Var "x") `shouldBe` ["x"]
+  
+    it "Does not return bound vars" $ do
+      freeVarsOf (Abs "x" (Var "x")) `shouldBe` []
+
+    it "Returns nested simple vars" $ do
+      freeVarsOf (Abs "x" (Var "y")) `shouldBe` ["y"]
+
+    it "Returns applied simple vars" $ do
+      freeVarsOf (App (Var "x") (Var "y")) `shouldBe` ["x", "y"]
