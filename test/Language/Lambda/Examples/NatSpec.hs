ab7c607bcaba90fc3f8280e5c4c190ddd8e365f4
Sean Gillespie <sean@mistersg.net>
2016-10-18 18:56:30 -0400

    Add power examples

--- test/Language/Lambda/Examples/NatSpec.hs
+++ test/Language/Lambda/Examples/NatSpec.hs
@@ -55,11 +55,11 @@ spec = do
     describe "multiply" $ do
       -- multiply(m, n) = m * n
       --
-      -- multiply is is defined by applying add m times
+      -- multiply is defined by applying add m times
       -- multiply = \m n f x. m (n f x) x)
       --
-      -- Using eta conversion, we can omit the parameters f and x
-      -- multiply = \m n. m (n f)
+      -- Using eta conversion, we can omit the parameter x
+      -- multiply = \m n f. m (n f)
       it "multiply 0 2 = 0" $ do
         "(\\m n f. m (n f)) (\\f x. x) (\\f x. f (f x))"
           `shouldEvalTo` "\\f x. x"
@@ -75,3 +75,24 @@ spec = do
       it "multiply 1 n = n" $ do
         "(\\m n f. m (n f)) (\\f x. f x) n"
           `shouldEvalTo` "\\f x. n f x"
+
+    describe "power" $ do
+      -- The function power raises m to the power of n.
+      -- power(m, n) = m^n
+      --
+      -- power is defined by applying multiply n times
+      -- power = \m n f x. (n m) f x
+      --
+      -- Using eta conversion again, we can omit the parameter f
+      -- power = \m n = n m
+
+      -- NOTE: Here we use the first form to get more predictable
+      -- variable names. Otherwise, alpha conversion will choose a random
+      -- unique variable.
+      it "power 0 1 = 0" $ do
+        "(\\m n f x. (n m) f x) (\\f x. x) (\\f x. f x)"
+          `shouldEvalTo` "\\f x. x"
+
+      it "power 2 3 = 8" $ do
+        "(\\m n f x. (n m) f x) (\\f x. f (f x)) (\\f x. f (f (f x)))"
+          `shouldEvalTo` "\\f x. f (f (f (f (f (f (f (f x)))))))"
