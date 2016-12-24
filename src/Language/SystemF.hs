94ed6bfe94aa060b95f570ceb0d426c7cb247e26
Sean Gillespie <sean@mistersg.net>
2016-12-23 22:45:45 -0500

    Add some boilerplate for SystemF entrypoint

--- src/Language/SystemF.hs
+++ src/Language/SystemF.hs
@@ -1,7 +1,15 @@
 module Language.SystemF (
+  PrettyPrint(..),
+  SystemFExpr(..),
   evalString
   ) where
 
+import Text.Parsec
+
+import Language.Lambda.Util.PrettyPrint
+import Language.SystemF.Expression
+
 -- TODO
+evalString :: String -> Either ParseError (SystemFExpr String String)
 evalString = undefined
 
